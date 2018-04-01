0df40b0466f5ec0b1b1f9569281a2c0241422b90
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 18:31:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-    planeteer_calls.map! {|call| call.capitalize }
+    planeteer_calls.map! { |call| call.capitalize }
+    planeteer_calls.each { |call| puts call + '!' }
 
 end
 
