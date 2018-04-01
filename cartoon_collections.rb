925bb6a6871cd24522c565364302c50634bf48e1
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 18:39:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(words)
     words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese(list)# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
