67acb65031ff6e19d09d4521680a506c87db3c17
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 18:27:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,7 @@ def roll_call_dwarves(dwarves)
     end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
 end
 
 def long_planeteer_calls# code an argument here
