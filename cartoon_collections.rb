044e6d1018ef90a12babb19ea4e7ecc344104e45
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 18:37:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,6 @@ end
 def summon_captain_planet(planeteer_calls)
     planeteer_calls.map! { |call| call.capitalize }
     planeteer_calls.collect { |call| call + '!' }
-    end
 end
 
 def long_planeteer_calls(words)
