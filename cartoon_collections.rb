38f62aecd88373154b804b7e8adf7133c73e2b0d
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 18:32:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
     planeteer_calls.map! { |call| call.capitalize }
-    planeteer_calls.each { |call| puts call + '!' }
+    planeteer_calls.each { |call| call + '!' }
 end
 
 def long_planeteer_calls# code an argument here
