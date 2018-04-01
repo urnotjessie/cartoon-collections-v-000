1a504c342ff2ae0c7e3c57560978d1ef0e8be58a
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 18:38:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-    words.any
+    words.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
